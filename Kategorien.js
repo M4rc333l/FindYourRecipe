189f377ac1efd8ddcb8b905f61a5e7709a2aa693
M4rc333l <marcelweber180@gmail.com>
2023-01-05 14:30:50 +0100

    Bugfix

--- Kategorien.js
+++ Kategorien.js
@@ -7,19 +7,36 @@ function buttonReady(bildRequired, idName){
         id = "button";
     }
     if(bildRequired==null && id !== "suchen"){
-        document.querySelector('#'+id).disabled = !((document.getElementById('firstCheckbox').checked || document.getElementById('secondCheckbox').checked || document.getElementById('thirdCheckbox').checked
-                || document.getElementById('fourthCheckbox').checked || document.getElementById('fifthCheckbox').checked || document.getElementById('sixthCheckbox').checked)
-            && document.getElementsByName("zutaten")[0].value.trim().length>0 && document.getElementsByName("zubereitung")[0].value.trim().length>0);
+        document.querySelector('#'+id).disabled =
+            !((document.getElementById('firstCheckbox').checked
+                || document.getElementById('secondCheckbox').checked
+                || document.getElementById('thirdCheckbox').checked
+                || document.getElementById('fourthCheckbox').checked
+                || document.getElementById('fifthCheckbox').checked
+                || document.getElementById('sixthCheckbox').checked)
+                && document.getElementsByName("zutaten")[0].value.trim().length>0
+                && document.getElementsByName("zubereitung")[0].value.trim().length>0);
     }
     else if(bildRequired!=null && id === "button") {
         let file = document.getElementById("bild").files;
-        document.querySelector('#'+id).disabled = !((document.getElementById('firstCheckbox').checked || document.getElementById('secondCheckbox').checked || document.getElementById('thirdCheckbox').checked
-                || document.getElementById('fourthCheckbox').checked || document.getElementById('fifthCheckbox').checked || document.getElementById('sixthCheckbox').checked) && file.length>0
-            && document.getElementsByName("zutaten")[0].value.trim().length>0 && document.getElementsByName("zubereitung")[0].value.trim().length>0);
+        document.querySelector('#'+id).disabled =
+            !((document.getElementById('firstCheckbox').checked
+                    || document.getElementById('secondCheckbox').checked
+                    || document.getElementById('thirdCheckbox').checked
+                    || document.getElementById('fourthCheckbox').checked
+                    || document.getElementById('fifthCheckbox').checked
+                    || document.getElementById('sixthCheckbox').checked) && file.length>0
+                    && document.getElementsByName("zutaten")[0].value.trim().length>0
+                    && document.getElementsByName("zubereitung")[0].value.trim().length>0);
     }
     if(idName==="suchen"){
-        document.querySelector('#'+id).disabled = !((document.getElementById('firstCheckbox').checked || document.getElementById('secondCheckbox').checked || document.getElementById('thirdCheckbox').checked
-            || document.getElementById('fourthCheckbox').checked || document.getElementById('fifthCheckbox').checked || document.getElementById('sixthCheckbox').checked));
+        document.querySelector('#'+id).disabled =
+            !((document.getElementById('firstCheckbox').checked
+                || document.getElementById('secondCheckbox').checked
+                || document.getElementById('thirdCheckbox').checked
+                || document.getElementById('fourthCheckbox').checked
+                || document.getElementById('fifthCheckbox').checked
+                || document.getElementById('sixthCheckbox').checked));
     }
     if(document.getElementById("firstCheckbox").checked || document.getElementById("secondCheckbox").checked){
         document.getElementById("thirdCheckbox").disabled = true;
